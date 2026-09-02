3de8460400c0df933f4e389044acfbfb6bd245be
Caner Ulug <canerulug@Caners-MacBook-Pro.local>
2026-09-02 12:49:49 +0200

    initial commit

---
+++ src/main/java/com/calmpainter/calm/painter/CalmPainterApplication.java
@@ -0,0 +1,13 @@
+package com.calmpainter.calm.painter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmPainterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmPainterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calmpainter/calm/painter/CalmPainterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmpainter.calm.painter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmPainterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
