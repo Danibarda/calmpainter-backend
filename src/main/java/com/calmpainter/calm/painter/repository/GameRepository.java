5c19caaf23bbe9238ecbf29c12f3197b3caed507
Kippzta <erik.rehnberg@hotmail.com>
2026-09-09 13:03:30 +0200

    added method to check if there is a game active or not

--- src/main/java/com/calmpainter/calm/painter/repository/GameRepository.java
+++ src/main/java/com/calmpainter/calm/painter/repository/GameRepository.java
@@ -1,8 +1,11 @@
 package com.calmpainter.calm.painter.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.calmpainter.calm.painter.model.Game;
+import com.calmpainter.calm.painter.model.GameState;
 
 public interface GameRepository extends MongoRepository<Game,String> {
-    
+    Optional<Game> findByState(GameState state);
 }
